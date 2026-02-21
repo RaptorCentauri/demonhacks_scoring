1536797158ee0e850443adca38b9f72ae6517e3d
Dave Shilander <dave.leo.shilander@icloud.com>
2026-02-20 22:36:54 -0600

    save work - sanitize

--- score.py
+++ score.py
@@ -2,6 +2,7 @@ import math
 
 import pandas as pd
 from collections import namedtuple
+from pprint import pprint
 
 NEEDED = [
 "Judge Name",
@@ -114,11 +115,11 @@ for pod, projects in pod_project_totals.items():
 
 
 # print it
-for pod, items in pod_scores_by_pod.items():
-    print("Pod:", pod)
-    for project, score, count in items:
-        print(f"  Project: {project}  Pod Score: {score}")
-    print()
+# for pod, items in pod_scores_by_pod.items():
+#     # print("Pod:", pod)
+#     for project, score, count in items:
+#         print(f"  Project: {project}  Pod Score: {score}")
+#     print()
 
 
 
@@ -127,47 +128,73 @@ for pod, items in pod_scores_by_pod.items():
 def calculate_num_projects_per_pod(n,r, p):
     return math.ceil((n * r) / p)
 
-
+project_count = 57
 pod_list = ["A", "B", "C", "D", "E", "F"]
-pj_list = list(range(1,60+1))
+pj_list = list(range(1,project_count+1))
+required_review_count = 3
 
 def distribute_projects(project_list):
     n = len(project_list) # number of projects
     p = len(pod_list) #total pod count
-    r = 3 # reviews per pod
+    r = required_review_count # reviews per pod
     l =  calculate_num_projects_per_pod(n,r,p)
-    print("L= ", l)
 
     workload = (n * r) / p
     shift = math.ceil(l / r)
 
-
-
     begin = 0
     end = l
 
-    print("Shift", shift)
+    assignments = {}
+
 
     for i in range(0, len(pod_list)):
+        assigned_projects = []
+
         if end > len(project_list):
             part_one = project_list[begin:end]
             remainder = l - len(part_one)
             part_two = project_list[0: remainder]
             combined = part_two + part_one
-            print(f"pod {pod_list[i]}: {combined}, count: {len(combined)}")
+            assigned_projects = combined
         else:
-            print(f"pod {pod_list[i]}: {project_list[begin:end]}, count: {len(project_list[begin:end])}")
+            assigned_projects = project_list[begin:end]
+        # print(f"pod {pod_list[i]}: {assigned_projects}, count: {len(assigned_projects)}")
+        assignments[pod_list[i]] = assigned_projects
+
         begin = begin+shift
         end = end+shift
 
-
-
-        #handle wrap around
+    return assignments
 
 
 
 
-distribute_projects(pj_list)
+def sanitize_project_assignments(assigned, project_list, r):
+    review_counts = {project: 0 for project in project_list}
+
+    for assigned_project in assigned:
+        assigned_project_list = assigned[assigned_project]
+        for project in assigned_project_list[:]:
+            if review_counts[project] < r:
+                review_counts[project] += 1
+            else:
+                assigned_project_list.remove(project)
+
+
+
+
+assigned = distribute_projects(pj_list)
+
+for project in assigned:
+    print(f"{project} : {assigned[project]}")
+
+sanitize_project_assignments(assigned, pj_list, required_review_count)
+
+print("SANITIZED")
+
+for project in assigned:
+    print(f"{project} : {assigned[project]}")
 
 
 
